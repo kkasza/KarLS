18d3718697a8313bd839c1ae367675195932c5fa
Karoly Kasza <kaszak@gmail.com>
2024-12-15 16:35:13 +0100

    Separated kernel version for cmpl and pkg/iso, more flexible. Updated both kernel versions to 6.12.5 LTS

--- pkg/pkg/kernel/6.12.5-karls-generic/kernel.mk
+++ pkg/pkg/kernel/6.12.5-karls-generic/kernel.mk
@@ -1,9 +1,14 @@
-KERNEL_SVER:=6.6.63
+KERNEL_SVER:=6.12.5
 KVER:=$(NAME)-$(KFLAV)
 KERNEL_VER_CUR:=$(KERNEL_SVER)-$(KVER)
 KERNEL_VER:=kernel-$(KERNEL_VER_CUR)
 $(KERNEL_VER)-REALDIR:=linux-$(KERNEL_SVER)
+$(KERNEL_VER)-URL:=https://cdn.kernel.org/pub/linux/kernel/v6.x
+$(KERNEL_VER)-REALFILE:=linux-$(KERNEL_SVER).tar.xz
+$(KERNEL_VER)-FILE:=$(KERNEL_VER).tar.xz
+$(KERNEL_VER)-SHA256:=39207fce1ce42838e085261bae0af5ce4a0843aa777cfc0f5c49bc7729602bcd
 
+SRC_LIST+=KERNEL
 PKG_LIST+=kernel
 
 KERNEL_PLH:=__karls__
@@ -16,9 +21,14 @@ KBUILD:=$($(KERNEL_VER)-REALDIR)-$(T)-$(KFLAV)
 
 kernel: $(BLD)/$(KERNEL_VER)-$(T).kp
 
-src/$(KERNEL_VER):
-	mkdir -p src
-	ln -s $(CMPL)/src/$($(KERNEL_VER)-REALDIR) $@
+krnl:
+	@echo $(KERNEL_VER)
+	@echo $($(KERNEL_VER)-URL)
+	@echo $($(KERNEL_VER)-FILE)
+
+#src/$(KERNEL_VER):
+#	mkdir -p src
+#	ln -s $(CMPL)/src/$($(KERNEL_VER)-REALDIR) $@
 
 .PHONY: $(BLD)/$(KERNEL_VER)-kconfig
 
