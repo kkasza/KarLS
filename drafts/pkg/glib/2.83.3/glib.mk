be99350e2646afbc6b906d6ac389250b7bf22618
Karoly Kasza <kaszak@gmail.com>
2025-02-14 09:07:21 +0100

    glib requires meson 1.4 to build, debian has 1.0, still in draft

--- drafts/pkg/glib/2.83.3/glib.mk
+++ drafts/pkg/glib/2.83.3/glib.mk
@@ -1,26 +1,26 @@
+GLIB_VER_CUR:=2.83.3
+GLIB_VER:=glib-$(GLIB_VER_CUR)
+$(GLIB_VER)-URL:=https://download.gnome.org/sources/glib/2.83
+$(GLIB_VER)-FILE:=$(GLIB_VER).tar.xz
+$(GLIB_VER)-SHA256:=d0c65318bb2e3fa594277cf98a71cffaf5f666c078db39dcec121757b2ba328d
 
-### DRAFT - meson required
+SRC_LIST+=GLIB
+PKG_LIST+=glib
 
-#GLIB_VER_CUR:=2.83.3
-#GLIB_VER:=glib-$(GLIB_VER_CUR)
-#$(GLIB_VER)-URL:=https://download.gnome.org/sources/glib/2.83
-#$(GLIB_VER)-FILE:=$(GLIB_VER).tar.xz
-#$(GLIB_VER)-SHA256:=d0c65318bb2e3fa594277cf98a71cffaf5f666c078db39dcec121757b2ba328d
+GLIB_OPTS:=--prefix=$(CMPL_INST) \
+--buildtype=release \
+-D introspection=disabled \
+-D glib_debug=disabled \
+-D man-pages=disabled \
+-D sysprof=disabled
 
-#SRC_LIST+=GLIB
-#PKG_LIST+=glib
+glib: $(BLD)/$(GLIB_VER)-$(T).kp
 
-#GLIB_OPTS:=--host=$(TARGET-ARCH) \
-#--prefix=$(CMPL_INST) \
-#--with-sysroot=$(CMPL_INST) \
-#$(XCCACHE) \
-#$(HCCACHE)
-
-#glib: $(BLD)/$(GLIB_VER)-$(T).kp
-
-#$(BLD)/$(GLIB_VER): src/$(GLIB_VER)
-#	mkdir -p $@/_kp_tmp/FILES/usr/lib $(BLD)/xpc
-#	cd $@; $(XPATH) $(XPCF) ../../src/$(GLIB_VER)/configure $(LIBNL_OPTS)
+$(BLD)/$(GLIB_VER): src/$(GLIB_VER)
+	mkdir -p $@/_kp_tmp/FILES/usr/bin $(BLD)/xpc
+	cp pkg/glib/$(GLIB_VER_CUR)/glib-cross.txt $@
+	cd $@; $(XPATH) $(XPCF) meson setup --cross-file glib-cross.txt  ../../src/$(GLIB_VER) $(GLIB_OPTS)
+#	cd $@; $(XPATH) $(XPCF) ../../src/$(GLIB_VER)/configure $(GLIB_OPTS)
 #	$(XPATH) $(MAKE) -C $@ V=1
 #	cp -rP src/$(LIBNL_VER)/include/netlink $(CMPL_INST)/include/
 #	$(STRIP) $@/lib/.libs/*.so*
