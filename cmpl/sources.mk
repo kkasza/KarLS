29ce500cd31566325dd5ad0568a63de450e06ed7
Karoly Kasza <kaszak@gmail.com>
2025-04-04 16:29:38 +0200

    Updated mpfr to 4.2.2 and kernel to 6.12.21 on toolchain and iso

--- cmpl/sources.mk
+++ cmpl/sources.mk
@@ -13,10 +13,10 @@ $(GMP_VER)-URL:=https://ftpmirror.gnu.org/gnu/gmp
 $(GMP_VER)-FILE:=$(GMP_VER).tar.xz
 $(GMP_VER)-SHA256:=a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898
 
-MPFR_VER:=mpfr-4.2.1
+MPFR_VER:=mpfr-4.2.2
 $(MPFR_VER)-URL:=https://ftpmirror.gnu.org/gnu/mpfr
 $(MPFR_VER)-FILE:=$(MPFR_VER).tar.xz
-$(MPFR_VER)-SHA256:=277807353a6726978996945af13e52829e3abd7a9a5b7fb2793894e18f1fcbb2
+$(MPFR_VER)-SHA256:=b67ba0383ef7e8a8563734e2e889ef5ec3c3b898a01d00fa0a6869ad81c6ce01
 
 MPC_VER:=mpc-1.3.1
 $(MPC_VER)-URL:=https://ftpmirror.gnu.org/gnu/mpc
@@ -31,10 +31,10 @@ $(LINUX_VER)-FILE:=$(LINUX_VER).tar.gz
 $(LINUX_VER)-REALDIR:=linux-8e1110a580887f4b82303b9354c25d7e2ff5860e
 $(LINUX_VER)-SHA256:=e3244061e44426eafe97541633b87a71b8be1828d1bc48a18ec4c83fddb2f4c5
 else
-LINUX_VER:=linux-6.12.16
+LINUX_VER:=linux-6.12.21
 $(LINUX_VER)-URL:=https://cdn.kernel.org/pub/linux/kernel/v6.x
 $(LINUX_VER)-FILE:=$(LINUX_VER).tar.xz
-$(LINUX_VER)-SHA256:=5f81362a694f51520bff9faecb73f1cc9bc7bece6fdd10d5c27e348df39d7dc4
+$(LINUX_VER)-SHA256:=9d1ae39a2ea024d99646f645fdbbbfa4545577132ba2643e01df75e32246d6c7
 endif
 
 MUSL_VER:=musl-1.2.5
